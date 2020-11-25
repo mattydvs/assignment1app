0a9c6decaded82f6fa243141ee626149c460b3e9
mattydvs <m.davis11@nuigalway.ie>
2020-11-25 22:11:24 +0000

    Initial version

---
+++ src/main/java/assignment1app/TestCode.java
@@ -0,0 +1,12 @@
+package assignment1app;
+
+import assignment.Student;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Wriite student code and then push
+		
+	}
+
+}
