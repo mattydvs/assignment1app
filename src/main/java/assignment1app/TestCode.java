c58ee355d65cec87ad314cfea74de2b406a52fe2
mattydvs <m.davis11@nuigalway.ie>
2020-11-26 13:09:51 +0000

    Testing

--- src/main/java/assignment1app/TestCode.java
+++ src/main/java/assignment1app/TestCode.java
@@ -1,12 +1,18 @@
 package assignment1app;
 
+import assignment.Module;
 import assignment.Student;
 
 public class TestCode {
 
 	public static void main(String[] args) {
 		// TODO Write student code and then push
-		Student jimbob;
+		
+		//create students
+		Student student2 = new Student("Alan Fitzpatrick", 17 / 01 / 1998, 22, 12345);
+		
+		
+		Module eng = new Module("Engineering", 12345);
 	}
 
 }
